74adacf7bfaa5e77e62e6a79a56fe469c6ffffce
Simon(Codex) <blank@blank.com>
2026-05-27 20:03:54 +0200

    hotfix

--- backend/src/public/public.module.ts
+++ backend/src/public/public.module.ts
@@ -4,9 +4,10 @@ import { PublicController } from './public.controller';
 import { AuthModule } from '../auth/auth.module';
 import { MessagesModule } from '../messages/messages.module';
 import { AttachmentsModule } from '../attachments/attachments.module';
+import { TicketsModule } from '../tickets/tickets.module';
 
 @Module({
-  imports: [AuthModule, MessagesModule, AttachmentsModule],
+  imports: [AuthModule, MessagesModule, AttachmentsModule, TicketsModule],
   providers: [PublicService],
   controllers: [PublicController],
 })
